54aa8cc725d5efca374446fb3ea940b29e710577
esorylul <esorylul@outlook.com>
2021-02-16 21:37:00 +0800

    gentooLinux

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int min_width = 400;                    /* minimum width when centered */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
@@ -14,8 +14,8 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#4C566A" },
-	[SchemeSelHighlight] = { "#FF8000", "#88c0d0" },
-	[SchemeNormHighlight] = { "#FF8000", "#4C566A" },
+	[SchemeSelHighlight] = { "#50fa7b", "#733a96" },
+	[SchemeNormHighlight] = { "#50fa7b", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -26,3 +26,6 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 4;
